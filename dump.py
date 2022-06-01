ce5096224901444296786804c52d8cec69705ca4
Mafia-killer8080 <96686213+Mafia-killer8080@users.noreply.github.com>
2022-06-02 03:43:26 +0500

    Update dump.py

--- dump.py
+++ dump.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from dz import _login
+    from dump import _login
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     _login()
